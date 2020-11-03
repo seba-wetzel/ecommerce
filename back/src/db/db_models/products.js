19cb2bf560ac0a953c819e2fbe107ede4ac19154
seba-wetzel <seba.wetzel@gmail.com>
2020-11-03 19:28:30 -0300

    mongo conectado

--- back/src/db/db_models/products.js
+++ back/src/db/db_models/products.js
@@ -1,15 +1,15 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
 var productSchema = new Schema({
   nombre: { type: String, required: true },
-  categoria: { type: Schema.Types.ObjectId, ref: "Category", required: true },
-  precio: { type: Number, required: true },
-  stock: { type: Number, required: true },
-  imgURL: { type: Array, required: true },
+  categoria: { type: Schema.Types.ObjectId, ref: "Category" },
+  precio: { type: Number },
+  stock: { type: Number },
+  imgURL: { type: Array },
   disponibilidad: { type: Boolean, default: true },
 });
 
 const Product = mongoose.model("Product", productSchema);
 
-module.exports = Product;
+export default Product;
