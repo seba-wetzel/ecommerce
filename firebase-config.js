abe0226e1c38c096016aaa022532df6848d5648a
seba-wetzel <seba.wetzel@gmail.com>
2020-11-03 13:20:25 -0300

    configuracion base de firebase y react providers

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyBzk2OZavEhBqRzNDetZrlBrAm5hIjAMTY",
+    authDomain: "ecommerce-2020-b068c.firebaseapp.com",
+    databaseURL: "https://ecommerce-2020-b068c.firebaseio.com",
+    projectId: "ecommerce-2020-b068c",
+    storageBucket: "ecommerce-2020-b068c.appspot.com",
+    messagingSenderId: "828383040394",
+    appId: "1:828383040394:web:1f3f27ef8edc37b6da6e73",
+    measurementId: "G-FXWFEH2VDR"
+};
--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import withFirebaseAuth from './firebase/login'
+
+import firebase from "firebase/app";
+import "firebase/auth";
+//import config from '../../firebase-config';
+
+import {
+  FirebaseAuthProvider,
+  FirebaseAuthConsumer,
+  IfFirebaseAuthed,
+  IfFirebaseAuthedAnd
+} from "@react-firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBzk2OZavEhBqRzNDetZrlBrAm5hIjAMTY",
+  authDomain: "ecommerce-2020-b068c.firebaseapp.com",
+  databaseURL: "https://ecommerce-2020-b068c.firebaseio.com",
+  projectId: "ecommerce-2020-b068c",
+  storageBucket: "ecommerce-2020-b068c.appspot.com",
+  messagingSenderId: "828383040394",
+  appId: "1:828383040394:web:1f3f27ef8edc37b6da6e73",
+  measurementId: "G-FXWFEH2VDR"
+};
+
+function App({ user, signOut, signInWithGoogle }) {
+  console.log(user)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FirebaseAuthProvider firebase={firebase} {...firebaseConfig}>
+      <div className="App">
+        <header className="App-header">
+          <IfFirebaseAuthed>
+            <p>
+              {user ? user.displayName : null}
+            </p>
+            <button onClick={signOut}>logout</button>
+
+
+          </IfFirebaseAuthed>
+          <button onClick={signInWithGoogle}>login</button>
+
+        </header>
+      </div>
+    </FirebaseAuthProvider>
   );
 }
 
-export default App;
+export default withFirebaseAuth(App);
