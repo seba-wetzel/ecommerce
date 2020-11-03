0a874fa9a3ca7c3162fb6e7fae7a23ecb6bb3ec4
seba-wetzel <seba.wetzel@gmail.com>
2020-11-03 19:28:30 -0300

    mongo conectado

--- back/src/db/config/index.js
+++ back/src/db/config/index.js
@@ -1,5 +1,5 @@
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost/ecommerce", {
+import mongoose from "mongoose"
+export default mongoose.connect("mongodb://localhost/ecommerce", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+}).catch(e => console.log(e))
