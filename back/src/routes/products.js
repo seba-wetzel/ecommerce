d2d0e2e6792636f930863beff38a7f272b4003be
seba-wetzel <seba.wetzel@gmail.com>
2020-11-04 12:10:34 -0300

    products route temp

--- back/src/routes/products.js
+++ back/src/routes/products.js
@@ -1,9 +1,20 @@
 import express from 'express';
+import Product from '../db/db_models/products.js'
+
 
 const products = express.Router()
 
-products.get("/", (req, res) => {
-    res.send(`${req.originalUrl}`)
+products.get("/", async (req, res) => {
+    console.log("llego")
+    const results = await Product.find()
+    res.send(results)
 })
 
+products.post('/', async (req, res) => {
+    const { nombre } = req.body
+    console.log(nombre)
+
+    const product = await Product.create({ nombre })
+    res.status(201).send(product)
+})
 export default products
