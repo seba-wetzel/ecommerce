1d0a8d57cfb963a2da2c145ed1c20eddf01e8097
laramov <laramovsovich@gmail.com>
2020-11-04 17:28:52 -0300

    carpeta de action creators

---
+++ front/src/constants-actioncreators/action-creators/products.js
@@ -0,0 +1,64 @@
+import axios from "axios";
+import {
+  RECEIVE_PRODUCTS,
+  RECEIVE_SINGLE_PRODUCT,
+  ADD_PRODUCT,
+  DELETE_PRODUCT,
+  UPDATE_PRODUCT,
+} from "../constants";
+
+const receiveProducts = (products) => ({
+  type: RECEIVE_PRODUCTS,
+  products,
+});
+
+const receiveSingleProduct = (product) => ({
+  type: RECEIVE_SINGLE_PRODUCT,
+  product,
+});
+
+const addProduct = (product) => ({
+  type: ADD_PRODUCT,
+  product,
+});
+
+const deleteProduct = (product) => ({
+  type: DELETE_PRODUCT,
+  product,
+});
+
+const updateProduct = (product) => ({
+  type: UPDATE_PRODUCT,
+  product,
+});
+
+export const fetchProducts = () => (dispatch) =>
+  axios
+    .get("/api/products")
+    .then((res) => res.data)
+    .then((products) => dispatch(receiveProducts(products)));
+
+export const fetchProduct = (id) => (dispatch) =>
+  axios
+    .get(`/api/products/${id}`)
+    .then((res) => res.data)
+    .then((product) => dispatch(receiveSingleProduct(product)));
+
+// recordar validacion, esto solo deberia ser accesible para admin
+export const postProduct = () => (dispatch) =>
+  axios
+    .post("/api/products")
+    .then((res) => res.data)
+    .then((product) => dispatch(addProduct(product)));
+
+export const removeProduct = () => (dispatch) =>
+  axios
+    .delete(`/api/products/${id}`)
+    .then((res) => res.data)
+    .then((product) => dispatch(deleteProduct(product)));
+
+export const updatedProduct = () => (dispatch) =>
+  axios
+    .put(`/api/products/${id}`)
+    .then((res) => res.data)
+    .then((product) => dispatch(updateProduct(product)));
