3b9cf647aaf573bd00e7e8aa4df25814651ef0ab
ArmandoRico <armandoricosandoval.com>
2020-11-05 13:44:15 -0500

    seed

--- back/src/db/db_models/categories.js
+++ back/src/db/db_models/categories.js
@@ -1,15 +1,3 @@
-<<<<<<< HEAD:back/src/db/db_models/categorias.js
-const mongoose = require("mongoose");
-
-var categorySchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
-  productos: [{ type: Schema.Types.ObjectId, ref: "Product", required: true }], //tal vez no es required xq podria tener categorias vacias?
-});
-
-const Category = mongoose.model("Category", categorySchema);
-
-module.exports = Category;
-=======
 import mongoose from "mongoose";
 import "../config/index.js";
 const Schema = mongoose.Schema;
@@ -22,4 +10,3 @@ const categorySchema = new Schema({
 const Category = mongoose.model("Category", categorySchema);
 
 export default Category;
->>>>>>> master:back/src/db/db_models/categories.js
