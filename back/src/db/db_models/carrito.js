3b6588a5197f6a04b41b785e8622428b5aef3083
seba-wetzel <seba.wetzel@gmail.com>
2020-11-04 23:07:43 -0300

    ruta de carrito

--- back/src/db/db_models/carrito.js
+++ back/src/db/db_models/carrito.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 
 const cartSchema = new Schema({
   user: [{ type: Schema.Types.ObjectId, ref: "User", required: true }],
-  producto: [{ type: Schema.Types.ObjectId, ref: "Product" }],
+  producto: [{ producto: { type: Schema.Types.ObjectId, ref: "Product" }, cantidad: Schema.Types.Number }],
 });
 
 const Cart = mongoose.model("Cart", cartSchema);
