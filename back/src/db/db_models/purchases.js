255aa7b5b93f59822570c593f3d7f11be29b45f6
ArmandoRico <armandoricosandoval.com>
2020-11-05 15:17:47 -0500

    Search arm

--- back/src/db/db_models/purchases.js
+++ back/src/db/db_models/purchases.js
@@ -1,18 +1,3 @@
-<<<<<<< HEAD
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-var purchaseSchema = new Schema({
-  fecha: { type: Date, required: true },
-  monto: { type: Number, required: true },
-  producto: [{ type: Schema.Types.ObjectId, ref: "Product", required: true }],
-  user: [{ type: Schema.Types.ObjectId, ref: "User", required: true }],
-});
-
-const Purchase = mongoose.model("Purchase", purchaseSchema);
-
-module.exports = Purchase;
-=======
 import mongoose from "mongoose";
 import "../config/index.js"
 const Schema = mongoose.Schema;
@@ -27,4 +12,3 @@ const purchaseSchema = new Schema({
 const Purchase = mongoose.model("Purchase", purchaseSchema);
 
 export default Purchase;
->>>>>>> master
