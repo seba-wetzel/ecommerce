fbdedd51f3f1719c99af8d6a5f329cac717f6c35
laramov <laramovsovich@gmail.com>
2020-11-10 12:09:13 -0300

    cambios nuevos lara

--- front/src/components/NewProduct.jsx
+++ front/src/components/NewProduct.jsx
@@ -5,45 +5,47 @@ const NewProduct = () => {
     <div className="striped grey lighten-4">
       <h1>Agregar Producto</h1>
       <p>falta completar</p>
-      <div>
-        <form class="col s12">
-          <div class="row">
-            <div class="input-field col s12">
-              <textarea
-                id="productname"
-                class="materialize-textarea"
-              ></textarea>
-              <label for="productname">Nombre</label>
+      <div classname="container">
+        <div className="row">
+          <form class="col s12">
+            <div class="row">
+              <div class="input-field col s12">
+                <textarea
+                  id="productname"
+                  class="materialize-textarea"
+                ></textarea>
+                <label for="productname">Nombre</label>
+              </div>
             </div>
-          </div>
-          <div class="row">
-            <div class="input-field col s12">
-              <textarea
-                id="productdescription"
-                class="materialize-textarea"
-              ></textarea>
-              <label for="productdescription">Descripcion</label>
+            <div class="row">
+              <div class="input-field col s12">
+                <textarea
+                  id="productdescription"
+                  class="materialize-textarea"
+                ></textarea>
+                <label for="productdescription">Descripcion</label>
+              </div>
             </div>
-          </div>
-          <div class="row">
-            <div class="input-field col s12">
-              <textarea
-                id="productprice"
-                class="materialize-textarea"
-              ></textarea>
-              <label for="productprice">Precio</label>
+            <div class="row">
+              <div class="input-field col s12">
+                <textarea
+                  id="productprice"
+                  class="materialize-textarea"
+                ></textarea>
+                <label for="productprice">Precio</label>
+              </div>
             </div>
-          </div>
-        </form>
+          </form>
+        </div>
+        <br />
+        <a class="waves-effect waves-light btn #880e4f pink darken-4">
+          Agregar Producto
+        </a>{" "}
+        <a class="waves-effect waves-light btn #880e4f pink darken-4">
+          <i class="material-icons left">delete</i>
+        </a>
+        <br />
       </div>
-      <br />
-      <a class="waves-effect waves-light btn #880e4f pink darken-4">
-        Agregar Producto
-      </a>{" "}
-      <a class="waves-effect waves-light btn #880e4f pink darken-4">
-        <i class="material-icons left">delete</i>
-      </a>
-      <br />
     </div>
   );
 };
