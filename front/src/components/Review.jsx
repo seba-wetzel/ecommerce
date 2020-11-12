7abd325a5014e1fce3d2efb4b37fb8a13d2e7d74
Luciano Moyano <lucianoarielmoyano@outlook.com>
2020-11-12 15:08:31 -0300

    categorias listo

--- front/src/components/Review.jsx
+++ front/src/components/Review.jsx
@@ -1,69 +1,103 @@
-import React,{useState, useEffect} from 'react';
-import { useDispatch, useSelector} from 'react-redux'
-import { FaStar } from 'react-icons/fa'
-import {postComment, fetchComments} from '../redux/actions/comments'
-import axios from 'axios';
-
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { FaStar } from "react-icons/fa";
+import { postComment, fetchComments } from "../redux/actions/comments";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
 const Review = () => {
-    const [rating, setRating] = useState(null)
-    const [hover, setHover] = useState(null)
-    const [comentario, setComentario] = useState('')
+  const [rating, setRating] = useState(null);
+  const [hover, setHover] = useState(null);
+  const [comentario, setComentario] = useState("");
 
-    const producto = useSelector(state => state.products.selectProduct)
-    const usuario = useSelector(state => state.user)
-    console.log('USUARIOOOOOOO', usuario)
-    const comentarios = useSelector(state => state.comments)
-    const dispatch = useDispatch()    
+  const producto = useSelector((state) => state.products.selectProduct);
+  const usuario = useSelector((state) => state.user);
+  console.log("USUARIOOOOOOO", usuario);
+  const comentarios = useSelector((state) => state.comments);
+  const userRole = useSelector((state) => state.user.db.role);
+  let userAut = false;
+  if (userRole === "admin") {
+    userAut = true;
+  }
+  if (userRole === "superAdmin") {
+    userAut = true;
+  }
+  const dispatch = useDispatch();
 
-    const addComentario = () =>{
-        let newComentario = {
-            id: comentario.length,
-            name: comentario,
-            complete: false
-        }
-        dispatch(postComment(producto._id, comentario))
-        setComentario('')
+  const addComentario = () => {
+    let newComentario = {
+      id: comentario.length,
+      name: comentario,
+      complete: false,
+    };
+    dispatch(postComment(producto._id, comentario));
+    setComentario("");
+  };
+
+  useEffect(async () => {
+    if (producto._id) {
+      dispatch(fetchComments(producto._id));
     }
-    
-    useEffect (async ()=> {
-     if(producto._id){ 
-        dispatch(fetchComments(producto._id)) }
-    },[producto]
-     )  
+  }, [producto]);
 
-    
+  return (
+    <div>
+      {[...Array(5)].map((star, i) => {
+        const ratingValue = i + 1;
+        return (
+          <label>
+            <input
+              type="radio"
+              name="rating"
+              value={ratingValue}
+              onClick={() => setRating(ratingValue)}
+            />
+            <FaStar
+              margin={8}
+              size={50}
+              color={ratingValue <= (hover || rating) ? "#ffd600" : "#64b5f6"}
+              onMouseEnter={() => setHover(ratingValue)}
+              onMouseLeave={() => setHover(null)}
+            />
+          </label>
+        );
+      })}
 
-    return ( 
-        <div>
-                {[...Array(5)].map((star, i) => {
-                    const ratingValue = i+1
-                        return (
-                            
-                            <label>
-                                <input type='radio' name='rating' value={ratingValue}  onClick={() => setRating(ratingValue)}/>
-                                <FaStar margin={8} size={50} color={ratingValue <= (hover || rating) ? "#ffd600" : "#64b5f6"} onMouseEnter={() => setHover(ratingValue)} onMouseLeave={() => setHover(null)}/>
-                            </label>
-                            
-                        ) 
-                    })}
-                      
-                        <form onSubmit={(e) =>{e.preventDefault();addComentario()}} className="col s12">
-                            <div className="row">
-                                <div className="input-field col s6">
-                                    <i className="material-icons prefix">mode_edit</i>
-                                    <input onChange={(e) => setComentario(e.target.value)} value={comentario} id="icon_prefix2" className="materialize-textarea"></input>
-                                    <label for="icon_prefix2">Dejanos tu opinion</label>
-                                    <a className='btn #4a148c purple darken-4'>Enviar</a>
-                                </div>
-                            </div>
-                        </form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          addComentario();
+        }}
+        className="col s12"
+      >
+        {userRole ? (
+          <div className="row">
+            <div className="input-field col s6">
+              <i className="material-icons prefix">mode_edit</i>
+              <input
+                onChange={(e) => setComentario(e.target.value)}
+                value={comentario}
+                id="icon_prefix2"
+                className="materialize-textarea"
+              ></input>
+              <label for="icon_prefix2">Dejanos tu opinion</label>
+              <a
+                className="btn #4a148c purple darken-4"
+                onClick={(e) => {
+                  e.preventDefault();
+                  addComentario();
+                }}
+              >
+                Enviar
+              </a>
+            </div>
+          </div>
+        ) : (
+          <Link to="/login">Logueate para dejar tu comentario</Link>
+        )}
+      </form>
+    </div>
+  );
+};
 
-
-        </div>        
-     );
-}
-
-
- 
-export default Review
+export default Review;
